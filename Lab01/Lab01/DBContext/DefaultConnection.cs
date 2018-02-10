198a8836215dff28d1d79ee66d9ae5605fc4d217
PDGL1006517 <pablo_082006@hotmail.cpm>
2018-02-10 15:04:16 -0600

    Inicio del laboratorio No. 1

---
+++ Lab01/Lab01/DBContext/DefaultConnection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lab01.Models;
+
+namespace Lab01.DBContext
+{
+    public class DefaultConnection
+    {
+        private static volatile DefaultConnection Instance;
+        private static object syncRoot = new Object();
+
+        public List<Jugador> Jugadores = new List<Jugador>();
+        public int IDActual { get; set; }
+
+        private DefaultConnection()
+        {
+            IDActual = 0;
+        }
+
+        public static DefaultConnection getInstance
+        {
+            get
+            {
+                if (Instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (Instance == null)
+                        {
+                            Instance = new DefaultConnection();
+                        }
+                    }
+                }
+                return Instance;
+            }
+        }
+    }
+}
---
+++ Lab01/Lab01/Models/Jugador.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab01.Models
+{
+    public class Jugador
+    {
+        public int JugadorID { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public string Club { get; set; }
+
+        public string Posición { get; set; }
+
+        public double Sueldo { get; set; }
+    }
+}
