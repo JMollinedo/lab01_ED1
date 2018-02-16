1725a2f6cade745c9d17330a959ffaa9831121e6
PDGL1006517 <pablo_082006@hotmail.cpm>
2018-02-16 12:11:01 -0600

    Reinicio de laboratorio

---
+++ Laboratorio No 1/Models/Jugador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laboratorio_No_1.Models
+{
+    public class Jugador
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Club { get; set; }
+        public double Salary { get; set; }
+        public string position { get; set; }
+
+
+    }
+}
